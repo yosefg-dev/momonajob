58a561dc01209efee890c52127424fac297d8198
NegasiAbe <negasiabebe@gmail.com>
2023-05-03 20:11:10 +0200

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,4 @@
-/*import 'bootstrap/dist/css/bootstrap.css';
-//import db from '../database';
-import React from "react";
-import Navbar from '../components/Navbar'; 
-import Footer from '..components/Footer'
-
-export default function Home(props) {
-    return (
-        <>
-          <Navbar/>        
-          <div className={styles.containerImg}>
-            <div className={styles.container}>
-            </div>
-          </div>
-          <Footer/>
-        </>
-      )
-    }*/
-
-    
+  
 import styles from '../styles/Home.module.css';
 import db from '../database';
 import Card from '../components/Card';
